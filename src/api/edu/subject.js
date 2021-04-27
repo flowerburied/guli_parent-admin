352863f099b56aa24beb31a7f30bc8a0e9196e4d
flowerburied <2842511561@qq.com>
2021-04-27 15:46:17 +0800

    excel上传并展示106

---
+++ src/api/edu/subject.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+export default {
+
+    // 课程分类
+    getAllSubject() {
+        return request({
+
+            url: `/eduservice/subject/getAllSubject`,
+            method: "get",
+            // 条件对象，后端使用RequestBody获取数据
+            // data表示把对象转换json进行传递到接口里面
+
+        })
+    },
+
+
+
+}
