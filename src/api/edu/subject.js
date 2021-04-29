41a16e274eb22581afcbcea7eb2748d33789b2e6
flowerburied <2842511561@qq.com>
2021-04-29 16:45:40 +0800

    添加了富文本编辑器，116

--- src/api/edu/subject.js
+++ src/api/edu/subject.js
@@ -13,7 +13,17 @@ export default {
 
         })
     },
+    // 课程分类
+    getSubjectList() {
+        return request({
 
+            url: `/eduservice/subject/getAllSubject`,
+            method: "get",
+            // 条件对象，后端使用RequestBody获取数据
+            // data表示把对象转换json进行传递到接口里面
+
+        })
+    },
 
 
 }
